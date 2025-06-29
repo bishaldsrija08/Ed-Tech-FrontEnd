c359fb3bd2554eb3735ff48865a13032783b7d7b
bishaldsrija08 <bishaldsrijal1@gmail.com>
2025-06-29 10:12:56 +0545

    next js project setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <h1>header</h1>
         {children}
+        <h1>footer</h1>
       </body>
     </html>
   );
